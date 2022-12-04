4d7f167151c8da079c5fe30d63a5dd8e2da16581
aron edwards <aronedwards91@gmail.com>
2022-12-04 14:33:48 +0000

    add indexjs for npm

---
+++ index.js
@@ -0,0 +1,18 @@
+const index = require("./index.json");
+const activity = require("./activity.json");
+const name = require("./name.json");
+const rank = require("./rank.json");
+const status = require("./status.json");
+
+const utilitySenses = require("@random-tables/utility-senses");
+
+module.exports = {
+  ...index,
+  tableData: {
+    activity,
+    name,
+    rank,
+    status,
+  },
+  requirements: [utilitySenses],
+};
